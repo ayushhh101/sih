0641d1075e4c9123a9aeb76772935cd1f4258ce5
Ayush <ayushsawant28112005@gmail.com>
2024-08-31 12:12:24 +0530

    idk

---
+++ src/components/Grid.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import '../componentscss/Grid.css'
+const Grid = () => {
+  return (
+    <>
+    <div className="logomain">
+    <div className="logocontainer">
+    <div className="images">
+        <img src='plant 1.jpg'></img>
+    </div>
+    <div className="images">
+        <img src='plant 2.jpg'></img>
+    </div>
+    <div className="images">
+        <img src='plant 3.jpg'></img>
+    </div>
+    <div className="images">
+        <img src='plant 3.jpg'></img>
+    </div>
+    <div className="images">
+        <img src='plant 3.jpg'></img>
+    </div>
+    </div>
+    <div className="logocontainer">
+    <div className="images">
+        <img src='plant 1.jpg'></img>
+    </div>
+    <div className="images">
+        <img src='plant 2.jpg'></img>
+    </div>
+    <div className="images">
+        <img src='plant 3.jpg'></img>
+    </div>
+    <div className="images">
+        <img src='plant 3.jpg'></img>
+    </div>
+    <div className="images">
+        <img src='plant 3.jpg'></img>
+    </div>
+    </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Grid
