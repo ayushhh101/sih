03bc43972c06f637134023b79cf754b4cbf3daef
Ayush <ayushsawant28112005@gmail.com>
2024-08-31 15:03:22 +0530

    Made many changes

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,47 +1,95 @@
-import React from 'react'
-import '../componentscss/Grid.css'
+import React from 'react';
+import '../componentscss/Grid.css';
+
 const Grid = () => {
+  // Array containing image sources and their descriptions
+  const plants = [
+    {
+      src: 'plant 1.jpg',
+      name: 'Aloe Vera',
+      description: 'Aloe Vera - A succulent plant known for its medicinal and skin healing properties.',
+    },
+    {
+      src: 'plant 2.jpg',
+      name: 'Basil',
+      description: 'Basil - A herb widely used in cooking and traditional medicine.',
+    },
+    {
+      src: 'plant 3.jpg',
+      name: 'Lavender',
+      description: 'Lavender - Known for its calming scent and use in essential oils.',
+    },
+    {
+      src: 'plant 3.jpg',
+      name: 'Mint',
+      description: 'Mint - A fragrant herb often used in beverages and desserts.',
+    },
+    {
+      src: 'plant 3.jpg',
+      name: 'Rosemary',
+      description: 'Rosemary - An aromatic herb commonly used in cooking and herbal medicine.',
+    },
+    {
+      src: 'plant 1.jpg',
+      name: 'Aloe Vera',
+      description: 'Aloe Vera - A succulent plant known for its medicinal and skin healing properties.',
+    },
+    {
+      src: 'plant 2.jpg',
+      name: 'Basil',
+      description: 'Basil - A herb widely used in cooking and traditional medicine.',
+    },
+    {
+      src: 'plant 3.jpg',
+      name: 'Lavender',
+      description: 'Lavender - Known for its calming scent and use in essential oils.',
+    },
+    {
+      src: 'plant 3.jpg',
+      name: 'Mint',
+      description: 'Mint - A fragrant herb often used in beverages and desserts.',
+    },
+    {
+      src: 'plant 3.jpg',
+      name: 'Rosemary',
+      description: 'Rosemary - An aromatic herb commonly used in cooking and herbal medicine.',
+    },
+    {
+      src: 'plant 3.jpg',
+      name: 'Mint',
+      description: 'Mint - A fragrant herb often used in beverages and desserts.',
+    },
+    {
+      src: 'plant 3.jpg',
+      name: 'Rosemary',
+      description: 'Rosemary - An aromatic herb commonly used in cooking and herbal medicine.',
+    },
+
+  ];
+
   return (
     <>
-    <div className="logomain">
-    <div className="logocontainer">
-    <div className="images">
-        <img src='plant 1.jpg'></img>
-    </div>
-    <div className="images">
-        <img src='plant 2.jpg'></img>
-    </div>
-    <div className="images">
-        <img src='plant 3.jpg'></img>
-    </div>
-    <div className="images">
-        <img src='plant 3.jpg'></img>
-    </div>
-    <div className="images">
-        <img src='plant 3.jpg'></img>
-    </div>
-    </div>
-    <div className="logocontainer">
-    <div className="images">
-        <img src='plant 1.jpg'></img>
-    </div>
-    <div className="images">
-        <img src='plant 2.jpg'></img>
-    </div>
-    <div className="images">
-        <img src='plant 3.jpg'></img>
-    </div>
-    <div className="images">
-        <img src='plant 3.jpg'></img>
-    </div>
-    <div className="images">
-        <img src='plant 3.jpg'></img>
-    </div>
-    </div>
-    </div>
-    
+      <div className="header">
+        <h1>VIRTUAL GARDEN</h1>
+      </div>
+      <div className="logomain">
+        <div className="logocontainer">
+          {plants.map((plant, index) => (
+            <div className="images" key={index}>
+              <img src={plant.src} alt={`Plant ${index + 1}`} />
+              <div className="text-overlay">
+                <h3 className="plant-name">{plant.name}</h3>
+                <p className="plant-description">{plant.description}</p>
+                <button className="btn">Method of Cultivation</button>
+                <button className="btn">Diseases it Cures</button>
+                <button className="btn">Know More Info</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Grid
+export default Grid;
